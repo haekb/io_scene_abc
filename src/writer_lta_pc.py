1196fe3fe16d388505baa54523a3eada1371779e
Jake <haekbreen@gmail.com>
2020-05-18 16:42:36 -0700

    Clean up, and fix a few bugs
    Bug 1: Anim Dims being 0,0,0 will break movement in AVP2 Model Edit
    Bug 2: Weight node list actually just needs everything in there.

--- src/writer_lta_pc.py
+++ src/writer_lta_pc.py
@@ -28,7 +28,7 @@ class LTANode(object):
         self._depth = 0
         self._children = []
 
-    # Originally its own node type, but it's basically a namesless child..so eh.
+    # Originally its own node type, but it's basically a nameless child..so eh.
     def create_property(self, value=''):
         return self.create_child('', value)
 
@@ -183,54 +183,7 @@ class NodeWriter(object):
 # End Class
 
 class LTAModelWriter(object):
-
-    # OLD
-    @staticmethod
-    def _string_to_bytes(string):
-        return struct.pack('H{0}s'.format(len(string)), len(string), string.encode('ascii'))
-
-    # OLD
-    @staticmethod
-    def _vector_to_bytes(vector):
-        return struct.pack('3f', vector.x, vector.y, vector.z)
-
-    # OLD
-    @staticmethod
-    def _quaternion_to_bytes(quaternion):
-        return struct.pack('4f', quaternion.x, quaternion.y, quaternion.z, quaternion.w)
-
-    # OLD
-    def _transform_to_bytes(self, transform):
-        # TODO: this if fixed size, convert to bytes instead of bytearray
-        buffer = bytearray()
-        buffer.extend(self._vector_to_bytes(transform.location))
-        buffer.extend(self._quaternion_to_bytes(transform.rotation))
-        return bytes(buffer)
-
-    # OLD
-    @staticmethod
-    def _get_unique_strings(model):
-        strings = set()
-        strings.add(model.command_string)
-        strings.update([node.name for node in model.nodes])
-        strings.update([child_model.name for child_model in model.child_models])
-        strings.update([animation.name for animation in model.animations])
-        strings.update([keyframe.string for animation in model.animations for keyframe in animation.keyframes])
-        strings.discard('')
-        return strings
-
-    @staticmethod
-    def _vector_to_string(vector):
-        return "%f %f %f" % (vector.x, vector.y, vector.z)
-
-
     def write(self, model, path):
-        # class Section(object):
-        #     def __init__(self, name, data):
-        #         self.name = name
-        #         self.data = data
-
-
         # This is the main node! Everything is a child of this duder.
         root_node = LTANode('lt-model-0')
 
@@ -243,9 +196,15 @@ class LTAModelWriter(object):
         ab_list_node = on_load_cmds_node.create_child('anim-bindings')
         ab_container_node = ab_list_node.create_container()
         for i, anim_binding in enumerate(model.anim_bindings):
+            anim_dims = anim_binding.extents
+
+            # Earlier versions of Model Edit act weird if anim_dims is (0,0,0)
+            if (anim_dims.magnitude == 0.0):
+                anim_dims = Vector( (10.0, 10.0, 10.0) )
+
             ab_node = ab_container_node.create_child('anim-binding')
             ab_node.create_child('name', anim_binding.name)
-            ab_node.create_child('dims').create_property(anim_binding.extents)
+            ab_node.create_child('dims').create_property(anim_dims)
             ab_node.create_child('translation').create_property(anim_binding.origin)
             # Gotta look for this one!
             ab_node.create_child('interp-time', model.animations[i].interpolation_time)
@@ -271,16 +230,17 @@ class LTAModelWriter(object):
             # This is a unique list of bone names used in this piece
             bone_influences = []
 
+            # Ok...it's actually just a list of all bones in the majority of examples I've seen..
+            for node in model.nodes:
+                bone_influences.append(node.name)
+
             for lod in piece.lods:
                 for vertex in lod.vertices:
 
                     weights = []
 
                     for weight in vertex.weights:
-                        # TODO: Determine if I actually need to include all of their parent nodes..
                         node_name = model.nodes[weight.node_index].name
-                        bone_influences.append(node_name)
-                        bone_influences = list(set(bone_influences))
 
                         new_node_index = bone_influences.index(node_name)
 
@@ -479,146 +439,6 @@ class LTAModelWriter(object):
             s_root_node = root_node.serialize()
             f.write(s_root_node)
             print("Finished serializing node list!")
-
-        # ''' AnimBindings '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('I', len(model.anim_bindings)))
-        # for anim_binding in model.anim_bindings:
-        #     buffer.extend(self._string_to_bytes(anim_binding.name))
-        #     buffer.extend(self._vector_to_bytes(anim_binding.extents))
-        #     buffer.extend(self._vector_to_bytes(anim_binding.origin))
-        # sections.append(Section('AnimBindings', bytes(buffer)))
-        
-
-        # sections = []
-
-        # ''' Header '''
-        # lod_count = 1  # TODO: incorporate LODs in a later version
-        # lod_distances = []
-
-        # unique_strings = self._get_unique_strings(model)
-
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('I', 12))  # version
-        # buffer.extend(struct.pack('I', model.keyframe_count))
-        # buffer.extend(struct.pack('I', len(model.animations)))
-        # buffer.extend(struct.pack('I', len(model.nodes)))
-        # buffer.extend(struct.pack('I', len(model.pieces)))
-        # buffer.extend(struct.pack('I', len(model.child_models)))
-        # buffer.extend(struct.pack('I', model.face_count))
-        # buffer.extend(struct.pack('I', model.vertex_count))
-        # buffer.extend(struct.pack('I', model.weight_count))
-        # buffer.extend(struct.pack('I', model.lod_count))
-        # buffer.extend(struct.pack('I', len(model.sockets)))
-        # buffer.extend(struct.pack('I', len(model.weight_sets)))
-        # buffer.extend(struct.pack('I', len(unique_strings)))
-        # buffer.extend(struct.pack('I', sum([len(unique_string) for unique_string in unique_strings])))
-        # buffer.extend(self._string_to_bytes(model.command_string))
-        # buffer.extend(struct.pack('f', model.internal_radius))
-        # buffer.extend(struct.pack('I', len(lod_distances)))
-        # buffer.extend(bytes(bytearray(60)))
-
-        # for lod_distance in lod_distances:
-        #     buffer.extend(struct.pack('f', lod_distance))
-
-        # sections.append(Section('Header', bytes(buffer)))
-
-        # ''' Pieces '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('2I', model.weight_count, len(model.pieces)))
-
-        # for piece in model.pieces:
-        #     buffer.extend(struct.pack('H', piece.material_index))
-        #     buffer.extend(struct.pack('f', piece.specular_power))
-        #     buffer.extend(struct.pack('f', piece.specular_scale))
-        #     buffer.extend(struct.pack('f', piece.lod_weight))
-        #     buffer.extend(struct.pack('H', 0))
-        #     buffer.extend(self._string_to_bytes(piece.name))
-        #     for lod in piece.lods:
-        #         buffer.extend(struct.pack('I', len(lod.faces)))
-        #         for face in lod.faces:
-        #             for face_vertex in face.vertices:
-        #                 buffer.extend(struct.pack('2fH', face_vertex.texcoord.x, face_vertex.texcoord.y,
-        #                                           face_vertex.vertex_index))
-        #         buffer.extend(struct.pack('I', len(lod.vertices)))
-        #         for vertex in lod.vertices:
-        #             buffer.extend(struct.pack('H', len(vertex.weights)))
-        #             buffer.extend(struct.pack('H', vertex.sublod_vertex_index))
-        #             for weight in vertex.weights:
-        #                 co = weight.location
-        #                 buffer.extend(struct.pack('I4f', weight.node_index, co.x, co.y, co.z, weight.bias))
-        #             buffer.extend(struct.pack('3f', vertex.location.x, vertex.location.y, vertex.location.z))
-        #             buffer.extend(struct.pack('3f', vertex.normal.x, vertex.normal.y, vertex.normal.z))
-
-        # sections.append(Section('Pieces', bytes(buffer)))
-
-        # ''' Nodes '''
-        # buffer = bytearray()
-        # for node in model.nodes:
-        #     buffer.extend(self._string_to_bytes(node.name))
-        #     buffer.extend(struct.pack('Hb', node.index, node.flags))
-        #     # TODO: extract this out to a function
-        #     # TODO: not entirely surprising that this is wrong, the "bind_matrix" is probably relative to parent
-        #     # when it needs to be in world-space? or vice versa
-        #     for f in list(itertools.chain(*[row.to_tuple() for row in node.bind_matrix])):
-        #         buffer.extend(struct.pack('f', f))
-        #     buffer.extend(struct.pack('I', len(node.children)))
-
-        # buffer.extend(struct.pack('I', 0))  # TODO: weight set count, use BONE GROUPS
-
-        # sections.append(Section('Nodes', bytes(buffer)))
-
-        # ''' ChildModels '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('H', len(model.child_models)))
-        # for child_model in model.child_models:
-        #     buffer.extend(self._string_to_bytes(child_model.name))
-        #     buffer.extend(struct.pack('I', child_model.build_number))
-        #     for transform in child_model.transforms:
-        #         buffer.extend(self._transform_to_bytes(transform))
-        # sections.append(Section('ChildModels', bytes(buffer)))
-
-        # ''' Animation '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('I', len(model.animations)))
-        # for animation in model.animations:
-        #     buffer.extend(self._vector_to_bytes(animation.extents))
-        #     buffer.extend(self._string_to_bytes(animation.name))
-        #     buffer.extend(struct.pack('i', animation.unknown1))
-        #     buffer.extend(struct.pack('I', animation.interpolation_time))
-        #     buffer.extend(struct.pack('I', len(animation.keyframes)))
-        #     for keyframe in animation.keyframes:
-        #         buffer.extend(struct.pack('I', keyframe.time))
-        #         buffer.extend(self._string_to_bytes(keyframe.string))
-        #     for node_keyframe_transform_list in animation.node_keyframe_transforms:
-        #         for keyframe_transform in node_keyframe_transform_list:
-        #             buffer.extend(self._transform_to_bytes(keyframe_transform))
-        # sections.append(Section('Animation', bytes(buffer)))
-
-        # ''' Sockets '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('I', len(model.sockets)))
-        # for socket in model.sockets:
-        #     buffer.extend(struct.pack('I', socket.node_index))
-        #     buffer.extend(self._string_to_bytes(socket.name))
-        #     buffer.extend(self._quaternion_to_bytes(socket.rotation))
-        #     buffer.extend(self._vector_to_bytes(socket.location))
-        # sections.append(Section('Sockets', bytes(buffer)))
-
-        # ''' AnimBindings '''
-        # buffer = bytearray()
-        # buffer.extend(struct.pack('I', len(model.anim_bindings)))
-        # for anim_binding in model.anim_bindings:
-        #     buffer.extend(self._string_to_bytes(anim_binding.name))
-        #     buffer.extend(self._vector_to_bytes(anim_binding.extents))
-        #     buffer.extend(self._vector_to_bytes(anim_binding.origin))
-        # sections.append(Section('AnimBindings', bytes(buffer)))
-
-        # with open(path, 'wb') as f:
-        #     for idx, section in enumerate(sections):
-        #         f.write(self._string_to_bytes(section.name))
-        #         if idx + 1 == len(sections):
-        #             f.write(struct.pack('i', -1))
-        #         else:
-        #             f.write(struct.pack('i', len(section.data) + f.tell() + 4))
-        #         f.write(bytes(section.data))
+        # End With
+    # End Def
+# End Class
